38a00fd7d9b7cb542bf1bd8f3c57175428a72516
QUOCVINH\ASUS <qv892005@gmail.com>
2025-10-06 22:36:41 +0700

    Initial Remead frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1 style={{ color: "#4CAF50" }}>Chào mừng đến với React Vite App!</h1>
+      <p>Bạn đã khởi chạy thành công ứng dụng React bằng Vite 🎉</p>
+    </div>
+  );
+}
+
+export default App;
